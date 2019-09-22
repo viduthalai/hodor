0dc20cf6a5459a8affe3f0d660cf036f7b5f8fc7
alagappan m <alagappan.m@SW-LP07191.local>
2019-09-22 17:26:42 +0530

    integration and fixes

--- js/main.js
+++ js/main.js
@@ -4,20 +4,6 @@ $(document).ready(function() {
       this.activeTasks = [];
       this.taskLists = [];
       this.startGetResults;
-      this.optData = [
-        { label: "Option 1", value: "1" },
-        { label: "Option 2", value: "2" },
-        { label: "Option 3", value: "3" },
-        { label: "Option 4", value: "4" },
-        { label: "Option 5", value: "5" }
-      ];
-      this.taskLists = [
-        { label: "Option 1", title: "Option 1", value: "1" },
-        { label: "Option 2", title: "Option 2", value: "2" },
-        { label: "Option 3", title: "Option 3", value: "3" },
-        { label: "Option 4", title: "Option 4", value: "4" },
-        { label: "Option 5", title: "Option 5", value: "5" }
-      ];
       this.initEvents();
       this.getBranches();
     },
@@ -25,6 +11,7 @@ $(document).ready(function() {
       $("#select_brand").on("change", $.proxy(this.getProfiles, this));
       $("#profiles_list").on("change", $.proxy(this.getTaskLists, this));
       $("#run_submit").on("click", $.proxy(this.submitTasks, this));
+      $("#stop_submit").on("click", $.proxy(this.stopTasks, this));
       $("#reset_all").on("click", $.proxy(this.resetAll, this));
       $("#update_submit").on("click", $.proxy(this.updateRate, this));
       $("#task_lists")
@@ -34,43 +21,59 @@ $(document).ready(function() {
           includeSelectAllOption: true
         })
         .multiselect("updateButtonText");
+      $("#active_task_lists")
+        .multiselect({
+          allSelectedText: "All",
+          maxHeight: 200,
+          includeSelectAllOption: true
+        })
+        .multiselect("updateButtonText");
     },
     updateDropDowns: function(nodeId, val, type) {
+      var data = [];
       if (type === "multi") {
-        $("#" + nodeId).multiselect("dataprovider", val);
+        for (var i = 0; i < val.length; i++) {
+          obj = JSON.parse('{"label": "'+ val[i] +'", "title": "' + val[i] + '", "value": "' + val[i] +'"}')
+          data.push(obj)
+        }
+        console.log(data);
+        $("#" + nodeId).multiselect("dataprovider", data);
         $("#" + nodeId).multiselect("rebuild");
       } else {
+        $("#" + nodeId).append(
+          "<option value=" + "" + "> Select One </option>");
         $.map(val, function(x) {
           return $("#" + nodeId).append(
-            "<option value=" + x.value + ">" + x.label + "</option>"
+            "<option value=" + x + ">" + x + "</option>"
           );
         });
       }
     },
     getBranches: function() {
       $.ajax({
-        url: "http://dummy.restapiexample.com/api/v1/employees",
+        url: "http://localhost:8080/api/v1/hudor/getbranches",
         dataType: "json",
         method: "GET",
-        success: $.proxy(function(resposone) {
-          this.updateDropDowns("select_brand", this.optData, "single");
+        success: $.proxy(function(response) {
+          this.updateDropDowns("select_brand",response, "single");
           $("#select_brand")
             .parent()
             .removeClass("d-none");
         }, this)
       });
     },
+
     getProfiles: function(e) {
       var $this = $(e.currentTarget);
       var selectedBranch = $this.val();
       $this.attr("disabled", "disabled");
       $.ajax({
-        url: "http://dummy.restapiexample.com/api/v1/employees",
+        url: "http://localhost:8080/api/v1/hudor/getprofiles",
         dataType: "json",
         method: "GET",
         data: { selectedBranch: selectedBranch },
-        success: $.proxy(function() {
-          this.updateDropDowns("profiles_list", this.optData, "single");
+        success: $.proxy(function(response) {
+          this.updateDropDowns("profiles_list", response, "single");
           $("#profiles_list")
             .parent()
             .removeClass("d-none");
@@ -82,16 +85,15 @@ $(document).ready(function() {
       var selectedProfile = $this.val();
       $this.attr("disabled", "disabled");
       $.ajax({
-        url: "http://dummy.restapiexample.com/api/v1/employees",
+        url: "http://localhost:8080/api/v1/hudor/getalltasks/" + selectedProfile,
         dataType: "json",
         method: "GET",
-        data: { selectedBrand: selectedProfile },
-        success: $.proxy(function() {
+        success: $.proxy(function(response) {
           $("#task_lists, #run_submit")
             .parents("li")
             .removeClass("d-none");
 
-          this.updateDropDowns("task_lists", this.taskLists, "multi");
+          this.updateDropDowns("task_lists", response, "multi");
         }, this)
       });
     },
@@ -99,12 +101,21 @@ $(document).ready(function() {
       $(e.currentTarget).attr("disabled", "disabled");
       var selectedTasks = $("#task_lists").val();
       var self = this;
+      var tasks = "";
+      for (var i = 0; i < selectedTasks.length; i++) {
+        tasks = tasks + selectedTasks[i] + ","
+      }
+      tasks = tasks.slice(0, -1);
       $.ajax({
-        url: "http://dummy.restapiexample.com/api/v1/employees",
+        url: "http://localhost:8080/api/v1/hudor/runtask/"+ tasks,
         dataType: "json",
         method: "GET",
-        data: { selectedBrand: selectedTasks },
         success: $.proxy(function(response) {
+          $("#active_task_lists, #stop_submit")
+            .parents("li")
+            .removeClass("d-none");
+
+          this.updateDropDowns("active_task_lists", selectedTasks, "multi");
           self.getTasksResults(response);
           this.startGetResults = setInterval(function() {
             self.getTasksResults(response);
@@ -112,19 +123,40 @@ $(document).ready(function() {
         }, this)
       });
     },
+    stopTasks: function(e) {
+      $(e.currentTarget).attr("disabled", "disabled");
+      var selectedTasks = $("#active_task_lists").val();
+      var self = this;
+      var tasks = "";
+      for (var i = 0; i < selectedTasks.length; i++) {
+        tasks = tasks + selectedTasks[i] + ","
+      }
+      tasks = tasks.slice(0, -1);
+      $.ajax({
+        url: "http://localhost:8080/api/v1/hudor/stopTask/"+ tasks,
+        dataType: "json",
+        method: "GET",
+        success: $.proxy(function(response) {
+          if (response === "200") {
+            $("#stop_notify .alert-info").removeClass("d-none");
+          } else {
+            $("#stop_notify .alert-danger").removeClass("d-none");
+          }
+          setTimeout(function() {
+            $("#stop_notify .alert").addClass("d-none");
+          }, 5000);
+        }, this)
+      });
+    },
     updateRate: function() {
       var activeTask = $("#active_task").val();
       var rateValue = $("#rate_value").val();
       $.ajax({
-        url: "http://dummy.restapiexample.com/api/v1/employees",
+        url: "http://localhost:8080/api/v1/hudor/updateTaskRate/"+activeTask+"/"+rateValue,
         dataType: "json",
         method: "GET",
-        data: {
-          activeTask: activeTask,
-          rateValue: rateValue
-        },
         success: $.proxy(function(response) {
-          if (response.status === "ok") {
+          if (response === "200") {
             $("#notify .alert-info").removeClass("d-none");
           } else {
             $("#notify .alert-danger").removeClass("d-none");
@@ -137,7 +169,7 @@ $(document).ready(function() {
     },
     getTasksResults: function(response) {
       $.ajax({
-        url: "http://dummy.restapiexample.com/api/v1/employees",
+        url: "http://localhost:8080/api/v1/hudor/getActiveTasks",
         dataType: "json",
         method: "GET",
         // data: { selectedBrand: selectedTasks },
@@ -147,7 +179,7 @@ $(document).ready(function() {
             .removeClass("d-none");
           this.paintResults();
 
-          this.updateDropDowns("active_task", this.optData, "single");
+          this.updateDropDowns("active_task", response, "single");
           $("#update_block").removeClass("d-none");
         }, this)
       });
